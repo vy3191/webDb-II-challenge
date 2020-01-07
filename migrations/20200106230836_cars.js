31be785af4ebb5883a708b7dfac44f9bc6e8ae06
vy3191 <venkyafhs@gmail.com>
2020-01-06 23:26:04 -0500

    Added database schema using migrations

--- migrations/20200106230836_cars.js
+++ migrations/20200106230836_cars.js
@@ -2,9 +2,15 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("cars", (table) => {
      table.integer("id").notNull().unique().primary();
+     table.integer("vin").notNull().unique();
+     table.text("make").notNull().unique();
+     table.text("model").notNull().unique();
+     table.float("mileage")
+     table.text("transmission").notNull();
+     table.text("title").notNull();
   })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("cars");
 };
