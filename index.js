7fb8822195d27f6c1b4c5f4621342382d2a4f0bd
vy3191 <venkyafhs@gmail.com>
2020-01-06 23:09:25 -0500

    created knexfile.js, migrations, and db.js file data folder

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ server.use(express.json());
 server.use(helmet());
 server.use(logger('dev'));
 server.use("/api/cars", carRoutes);
-
+server.use((error, req, res, next) => {
+   res.status(500).json({
+      message: `Something went wrong with the server`
+   })
+})
 
 server.listen(PORT, () => {
    console.log(`\n=== Web API Listening on http://localhost:${PORT}===\n`);
--- routers/carRoutes.js
+++ routers/carRoutes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/db");
 
 router.get("/", (req,res) => {
-   res.json({msg:'working now'})
+  try {
+
+  }catch(error) {
+     next(error);
+  }
 });
 
 router.post("/", (req,res) => {
-   
+   try {
+
+   } catch(error) {
+       next(error); 
+   }
 })
 
 module.exports = router;
